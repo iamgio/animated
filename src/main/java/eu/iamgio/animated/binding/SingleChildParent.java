ce0141bc61ad03c880c6e183a1ab152fcbf80991
Giorgio Garofalo <giogar2705@gmail.com>
2023-06-17 16:57:28 +0200

    Allow changing SingleChildParent's child

--- src/main/java/eu/iamgio/animated/binding/SingleChildParent.java
+++ src/main/java/eu/iamgio/animated/binding/SingleChildParent.java
@@ -1,5 +1,6 @@
 package eu.iamgio.animated.binding;
 
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -10,24 +11,43 @@ import javafx.scene.Parent;
  */
 public class SingleChildParent extends Parent {
 
-    protected final SimpleObjectProperty<Node> child = new SimpleObjectProperty<>();
+    protected final ObjectProperty<Node> child = new SimpleObjectProperty<>();
 
-    protected SingleChildParent(Node child) {
+    protected SingleChildParent() {
         // Registers child listener
         this.child.addListener((observable, oldChild, newChild) -> {
-            if(newChild != null) {
+            if (newChild != null) {
                 getChildren().setAll(newChild);
             } else {
                 getChildren().clear();
             }
         });
+    }
+
+    protected SingleChildParent(Node child) {
+        this();
         this.child.set(child);
     }
 
     /**
-     * @return current child
+     * @return the current child
+     */
+    public ObjectProperty<Node> childProperty() {
+        return this.child;
+    }
+
+    /**
+     * @return the current child
      */
     public Node getChild() {
-        return child.get();
+        return this.child.get();
+    }
+
+    /**
+     * Sets the new child.
+     * @param child new child. If <tt>null</tt>, the current child is removed and nothing is added.
+     */
+    public void setChild(Node child) {
+        this.childProperty().set(child);
     }
 }
