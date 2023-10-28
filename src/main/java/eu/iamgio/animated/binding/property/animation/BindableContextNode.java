9ce1932f8bee1cdd96c119dd314a290c29bf3826
Giorgio Garofalo <giogar2705@gmail.com>
2023-10-28 11:59:05 +0200

    Add missing docs

--- src/main/java/eu/iamgio/animated/binding/property/animation/BindableContextNode.java
+++ src/main/java/eu/iamgio/animated/binding/property/animation/BindableContextNode.java
@@ -4,9 +4,12 @@ import javafx.beans.property.ObjectProperty;
 import javafx.scene.Node;
 
 /**
- *
+ * A container for a writable node property.
  */
 public interface BindableContextNode<N extends Node> {
 
+    /**
+     * @return the target node for the context
+     */
     ObjectProperty<N> targetNodeProperty();
 }
